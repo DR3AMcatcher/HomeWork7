ccb290ad02766fd870d37f4230b1db29327da846
apidgirets@sqad.com <apidgirets@sqad.com>
2018-12-09 16:59:11 -0600

    original commit

---
+++ HomeWork7/AdvancedSelenium.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace HomeWork7
+{
+    [TestFixture]
+    public class AdvancedSelenium : SetUpFixture
+    {
+        [Test]
+        public void Navigate()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+        }
+
+
+    }
+}
---
+++ HomeWork7/SetUp.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace HomeWork7
+{
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        public IWebDriver driver;
+        public string SaveFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "AlexPScreenshots");
+
+        [OneTimeSetUp]
+        public void CreateFolder()
+        {
+            if (!Directory.Exists(SaveFolder))
+            {
+                Directory.CreateDirectory(SaveFolder);
+            }
+        }
+        [OneTimeSetUp]
+        public void InitializeDriver()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown]
+        public void Clean()
+        {
+            driver.Quit();
+        }
+    }
+}
